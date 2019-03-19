63efe9a11a55235be7a50584a6ebf0fd989d13f9
Fifis <5990916+Fifis@users.noreply.github.com>
2019-03-19 16:58:43 +0100

    Now it works under Linux and Windows

--- smoothing-simulation-03-distribution.R
+++ smoothing-simulation-03-distribution.R
@@ -130,12 +130,12 @@ abline(v = dchisq(Xgrid[i], 3), lwd = 2, lty = 2, col = "blue")
 rug(fhat[i, ], col = "#00000011")
 dev.off()
 
-# Density estimator
 if (!("animation" %in% rownames(installed.packages()))) install.packages("animation")
 library(animation) # We can make a wireframe plot using this extra library
 options(bitmapType = "cairo")
 
-saveGIF({ # Takes roughly 30 seconds on Linux and a whopping 20 minutes on Windows because of the rug
+# Density estimator
+saveGIF({ # Might takes from 30 seconds to 5 minutes depenging on the performance of ImageMagick
   for (i in 1:101) {
     plot(den.grid, kernelDensity(fhat[i, ], den.grid, 0.0004), type = "l", main = paste0("Distribution of KDE at X=", round(Xgrid[i], 2)), bty = "n", lwd = 2, ylab = "Frequency across simulations", xlab = "Density")
     abline(v = dchisq(Xgrid[i], 3), lwd = 2, col = "blue", lty = 2)
@@ -143,11 +143,11 @@ saveGIF({ # Takes roughly 30 seconds on Linux and a whopping 20 minutes on Windo
     legend("topright", legend = c(expression(f[X](x)), expression(hat(f)[X](x))), col = c("blue", "black"), lty = c(2, 1), lwd = 2, bty = "n")
     if (i %% 10 == 0) cat(i, "\n")
   }
-}, movie.name = "34-density-estimators-the-film.gif", interval = 0.25, ani.width = 640, ani.height = 480, ani.dev = function(...) png(..., type="cairo"))
+}, movie.name = "34-density-estimators-the-film.gif", interval = 0.25, ani.width = 640, ani.height = 480, ani.dev = if (.Platform$OS.type=="windows") function(...) png(..., type="cairo") else "png")
 
-# Regression estimator with a slightly growing bandwidth
+# Regression estimator
 mu.grid <- seq(0, 27, length.out = 401)
-saveGIF({ # Takes roughly 30 seconds
+saveGIF({ # Might takes from 50 seconds to 5 minutes depenging on the performance of ImageMagick
   for (i in 1:101) {
     plot(mu.grid, kernelDensity(muhat[i, ], mu.grid, bw.nrd(muhat[i, ]) / 5), type = "l", main = paste0("Distribution of NW estimator at X =", round(Xgrid[i], 2)), lwd = 2, ylab = "Frequency across simulations", xlab = expression(mu))
     abline(v = mu(Xgrid[i]), lwd = 2, col = "red")
@@ -155,13 +155,13 @@ saveGIF({ # Takes roughly 30 seconds
     legend("topright", legend = expression(mu(x)), col = "red", lwd = 2, bty = "n")
     if (i %% 10 == 0) cat(i, "\n")
   }
-}, movie.name = "35-regression-estimators-the-film.gif", interval = 0.25, ani.width = 640, ani.height = 480, ani.dev = function(...) png(..., type="cairo"))
+}, movie.name = "35-regression-estimators-the-film.gif", interval = 0.25, ani.width = 640, ani.height = 480, ani.dev = if (.Platform$OS.type=="windows") function(...) png(..., type="cairo") else "png")
 
 # In 3D, we can use a coarser grig, but generating 101 densities still takes a while, so we parallelise while we can
 den.grid <- seq(0, 0.3, length.out = 201)
 kdes <- mclapply(1:101, function(i) kernelDensity(fhat[i, ], den.grid, bw.nrd(fhat[i, ]) / 5), mc.cores = ncores)
 kdes <- lapply(kdes, function(x) x / max(x)) # Normalising them to one for nice plotting
-saveGIF({ # Takes roughly 30 seconds on Linux but 20 minutes on Windows!
+saveGIF({ # Might takes from 20 seconds to 5 minutes depenging on the performance of ImageMagick
   for (a in 1:360) {
     par(mar = c(2, 2, 2, 2))
     p <- persp(c(0, 11), c(0, 0.3), matrix(rep(0, 4), 2), col = "white", zlim = c(0, 1.01), theta = a, phi = 40, xlab = "x", ylab = "Density", zlab = "KDE concentration")
@@ -169,12 +169,12 @@ saveGIF({ # Takes roughly 30 seconds on Linux but 20 minutes on Windows!
     for (i in 1:101) lines(trans3d(rep(Xgrid[i], length(den.grid)), den.grid, kdes[[i]], pmat = p), lwd = 1, col = "#00000077")
     if (a %% 5 == 0) cat(a, "/ 360\n")
   }
-}, movie.name = "36-kde-3d.gif", interval = 1 / 25, ani.width = 480, ani.height = 480, ani.dev = function(...) png(..., type="cairo"))
+}, movie.name = "36-kde-3d.gif", interval = 1 / 25, ani.width = 480, ani.height = 480, ani.dev = if (.Platform$OS.type=="windows") function(...) png(..., type="cairo") else "png")
 
 mu.grid <- seq(0, 27, length.out = 201)
 mus <- mclapply(1:101, function(i) kernelDensity(muhat[i, ], mu.grid, bw.nrd(muhat[i, ]) / 5), mc.cores = ncores)
 mus <- lapply(mus, function(x) x / max(x))
-saveGIF({ # Takes roughly 30 seconds on Linux but 20 minutes on Windows!
+saveGIF({ # Might takes from 20 seconds to 5 minutes depenging on the performance of ImageMagick
   for (a in 1:360) {
     par(mar = c(2, 2, 2, 2))
     p <- persp(c(0, 11), c(0, 25), matrix(rep(0, 4), 2), col = "white", zlim = c(0, 1.01), theta = a, phi = 40, xlab = "x", ylab = "mu", zlab = "Estimator density")
@@ -182,4 +182,4 @@ saveGIF({ # Takes roughly 30 seconds on Linux but 20 minutes on Windows!
     for (i in 1:101) lines(trans3d(rep(Xgrid[i], length(mu.grid)), mu.grid, mus[[i]], pmat = p), lwd = 1, col = "#00000077")
     if (a %% 5 == 0) cat(a, "/ 360\n")
   }
-}, movie.name = "37-mu-3d.gif", interval = 1 / 25, ani.width = 480, ani.height = 480, ani.dev = function(...) png(..., type="cairo"))
+}, movie.name = "37-mu-3d.gif", interval = 1 / 25, ani.width = 480, ani.height = 480, ani.dev = if (.Platform$OS.type=="windows") function(...) png(..., type="cairo") else "png")
